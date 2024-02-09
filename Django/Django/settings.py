3c9c0a7bd16d75b07f72792e1069093bee236580
ShawnZhong <shawnzhongchn@gmail.com>
2024-02-09 12:13:29 +0800

    ORM

--- Django/Django/settings.py
+++ Django/Django/settings.py
@@ -36,6 +36,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'Django_learn.apps.DjangoLearnConfig',
+    'music',
+    'bookstore'
 ]
 
 MIDDLEWARE = [
@@ -70,11 +72,14 @@ WSGI_APPLICATION = 'Django.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'mysite3',
+        'USER': 'shawn',
+        'PASSWORD': 'password',
+        'HOST': '127.0.0.1',
+        'PORT': '3306'
     }
 }
 
